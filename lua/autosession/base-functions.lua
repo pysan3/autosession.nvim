3a471d98113a71652efc30a0773ad7ca0d7f9946
pysan3 <pysan3@gmail.com>
2022-09-24 19:18:58 +0900

    added better function typing notation

--- lua/autosession/base-functions.lua
+++ lua/autosession/base-functions.lua
@@ -1,9 +1,9 @@
 local M = {}
 
 ---echos `msg` as a vim.notify
----@param msg string
----@param level string ("info", "warn", "error")
----@param ... any any options to pass to vim.notify
+---@param msg string: message to notify
+---@param level string | nil: ("info", "warn", "error"), if nil, "info" is used
+---@param ... any: Any options to pass to vim.notify
 M.echo = function(msg, level, ...)
   if level == nil then
     level = "info"
@@ -11,18 +11,18 @@ M.echo = function(msg, level, ...)
   vim.notify(msg, level, ...)
 end
 
----return if file exists
----@param path string
+---return true if file exists
+---@param path string: path to file
 ---@return boolean
 M.file_exist = function(path)
   local f = io.open(path, "r")
-  return f ~= nil and io.close(f)
+  return f ~= nil and io.close(f) == true
 end
 
 ---returns string where `t` is trimmed from both sides of `s`
----@param s string
----@param t string
----@return string
+---@param s string: base string
+---@param t string | nil: string to be trimmed, if nil, '%s' is used
+---@return string:
 M.s_trim = function(s, t)
   if not t then
     t = "%s"
@@ -31,17 +31,17 @@ M.s_trim = function(s, t)
 end
 
 ---same as running `basename "$path"` in command line
----@param path string
----@return string
+---@param path string: path to file
+---@return string: returns the basename of `path`
 M.basename = function(path)
-  return M.s_trim(path):gsub("(.*/)(.*)", "%2")
+  return string.gsub(M.s_trim(path), "(.*/)(.*)", "%2")
 end
 
 ---Confirm: asks for a confirmation in cmd
----@param msg string dont forget to add [y/N] or [Y/n] at the end
----@param ans string what would be the default option. usually 'y' or 'n'
----@param default boolean return value if <CR> was pressed without any letter
----@return boolean
+---@param msg string: dont forget to add [y/N] or [Y/n] at the end
+---@param ans string: what would be the default option. usually 'y' or 'n'
+---@param default boolean: return value if <CR> was pressed without any letter
+---@return boolean:
 M.Confirm = function(msg, ans, default)
   print(msg .. " ")
   local raw_ans = tonumber(vim.fn.getchar(), 10)
@@ -58,8 +58,8 @@ M.Confirm = function(msg, ans, default)
 end
 
 ---SessionName: returns the basename of `path`
----@param path string
----@return string
+---@param path string: path to cwd
+---@return string: basename of cwd
 M.SessionName = function(path)
   if path == "" then
     return ""
@@ -68,8 +68,8 @@ M.SessionName = function(path)
 end
 
 ---FullPath: returns `path`'s absolute path
----@param path string
----@return string
+---@param path string: relative path
+---@return string: absolute path of `path`
 M.FullPath = function(path)
   return vim.fn.resolve(vim.fn.expand(path))
 end
